987c07e3ba3b5e6c2c66d1d87fdd30d375c7e339
Nishan Tolang <ni.tolang88@gmail.com>
2023-08-24 01:40:21 +0300

    Second commit update mobile menu function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // write your JavaScript here
+const menuBtn = document.querySelector(".humburger");
+const mblMenu = document.querySelector(".mbl_menu");
+menuBtn.addEventListener("click", function () {
+  mblMenu.classList.toggle("is_active");
+});
